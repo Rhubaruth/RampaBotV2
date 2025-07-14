f2b7cbd0e715d5f7f11554d46238ba1db4539391
Rhubaruth <43817268+Rhubaruth@users.noreply.github.com>
2025-07-14 18:35:24 +0200

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,6 @@ if __name__ == "__main__":
 
     DC_TOKEN = get_discord_token()
     load_rest_api()
-        print("DOCKER DID UPDATE")
-    print("BLAAAAAAA AKDFJSLFJSKLDFJKDSJFOJQWEIOJWQEIOPFJEWOIFJOEWIJFOIESJFSJDKFJSIVSONIEWPRJ 54498546518941698")
 
-    # bot.run(DC_TOKEN)
+    print("===================================================================")
+    bot.run(DC_TOKEN)
