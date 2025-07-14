7ffb4cdaeb5ad335cfbe5a755bdc970d6c0e5632
Rhubaruth <43817268+Rhubaruth@users.noreply.github.com>
2025-07-14 18:33:36 +0200

    Update main.py - with compose down

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ if __name__ == "__main__":
 
     DC_TOKEN = get_discord_token()
     load_rest_api()
-    print("DOCKER DID UPDATE")
+        print("DOCKER DID UPDATE")
     print("BLAAAAAAA AKDFJSLFJSKLDFJKDSJFOJQWEIOJWQEIOPFJEWOIFJOEWIJFOIESJFSJDKFJSIVSONIEWPRJ 54498546518941698")
+
     # bot.run(DC_TOKEN)
