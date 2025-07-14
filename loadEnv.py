30fc9c31904412ecc45344b89fc71fb862c55858
rhubaruth <kubik.vana@gmail.com>
2025-07-14 15:34:03 +0200

    change loading of dotenv

--- loadEnv.py
+++ loadEnv.py
@@ -7,15 +7,19 @@ URL_REST_API = ""
 REST_API_KEY = ""
 
 
+def load_env():
+    if os.path.exists(env_path):
+        result = load_dotenv(env_path)
+        print(f"dotenv loading result: {result}")
+
+
 def load_rest_api():
     global URL_REST_API, REST_API_KEY
 
-    load_dotenv(env_path)
     URL_REST_API = os.getenv('URL_REST_API')
     REST_API_KEY = os.getenv('REST_API_KEY')
     return URL_REST_API, REST_API_KEY
 
 
 def get_discord_token():
-    load_dotenv(env_path)
     return os.getenv('DISCORD_TOKEN')
--- main.py
+++ main.py
@@ -1,8 +1,13 @@
-from loadEnv import get_discord_token, load_rest_api
+from loadEnv import (
+    load_env,
+    get_discord_token,
+    load_rest_api,
+)
 from botLoad import bot
 
 if __name__ == "__main__":
     print("hello world")
     DC_TOKEN = get_discord_token()
+    load_env()
     load_rest_api()
     bot.run(DC_TOKEN)
